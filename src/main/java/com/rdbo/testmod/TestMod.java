cc1dd27266cc26950c0e9cfe21af3aabec957d7f
rdbo <rdbodev@gmail.com>
2021-09-22 18:32:40 -0300

    update

--- src/main/java/com/rdbo/testmod/TestMod.java
+++ src/main/java/com/rdbo/testmod/TestMod.java
@@ -13,11 +13,15 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.BlockPos;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraft.item.ItemStack;
 
 @Mod(modid = TestMod.MODID, version = TestMod.VERSION)
 public class TestMod {
 	public static final String MODID = "testmod";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 
 	@EventHandler
 	public void init(FMLInitializationEvent event)
@@ -32,7 +36,21 @@ public class TestMod {
 			return;
 		}
 
+		EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
+		BlockPos pos = player.getPosition();
+		ItemStack item = player.getHeldItem();
+		String item_name = "";
+		if (item != null)
+			item_name = item.getDisplayName();
 		FontRenderer font_renderer = Minecraft.getMinecraft().fontRendererObj;
 		font_renderer.drawString(EnumChatFormatting.GREEN + "This is a test", 5, 5, 0);
+		font_renderer.drawString(
+			EnumChatFormatting.BLUE + "Player Pos: { " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ() + " }",
+			5, 5 + font_renderer.FONT_HEIGHT, 0
+		);
+		font_renderer.drawString(
+			EnumChatFormatting.RED + "Held Item: " + item_name,
+			5, 5 + font_renderer.FONT_HEIGHT * 2, 0
+		);
 	}
 }
